0aa5f02b5446a0ba1a7c56eed1a68e87bed27c5f
Dena Shink <dshink@iu.edu>
2025-04-05 01:37:58 -0400

    dont need these (using ollama and gemma3:1b)

--- react-chatbotify/cypress/fixtures/templates/pre_react_18/index.tsx
+++
@@ -1,9 +0,0 @@
- 
-// @ts-nocheck
-import React from "react";
-
-import { render } from "react-dom";
-import App from "./App";
-
-const rootElement = document.getElementById("root") as HTMLElement;
-render(<App />, rootElement);
